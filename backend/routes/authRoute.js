a24a0cf5610a4e12cfc0c604c2025156f750545b
Shivam8299 <shivammishra0381@gmail.com>
2025-03-29 23:40:12 +0530

    complete user register API

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,9 +2,9 @@ import express from 'express'
 const router = express.Router()
 import { signup,login,logout } from '../controllers/authController.js'
 
-router.get('/sign-up',signup)
-router.get('/login',login)
-router.get('/logout',logout)
+router.post('/sign-up',signup)
+router.post('/login',login)
+router.post('/logout',logout)
  
 
 export default router
